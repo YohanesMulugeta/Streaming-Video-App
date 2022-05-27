14f783d15aed974f5d464627d154b41d90edc837
Yohanes <yohanesmulugeta21@gmail.com>
2022-05-27 21:07:07 +0300

    remove everything I added for the purpose of test

--- src/components/App.js
+++ src/components/App.js
@@ -8,38 +8,16 @@ import StreamDelete from "./streams/StreamDelete";
 import StreamList from "./streams/StreamList";
 import Header from "./Header";
 
-const Nav = () => {
-  return (
-    <ul style={{ listStyle: "none" }}>
-      <li>
-        <Link to="/">Streams</Link>
-      </li>
-      <li>
-        <Link to="/streams/new">Create a Stream</Link>
-      </li>
-      <li>
-        <Link to="/streams/edit">Edit</Link>
-      </li>
-      <li>
-        <Link to="/streams/delete">Delete</Link>
-      </li>
-      <li>
-        <Link to="/streams/show">Show</Link>
-      </li>
-    </ul>
-  );
-};
-
 const App = () => {
   return (
     <div className="ui container">
       <BrowserRouter>
         <Header />
-        {/* <Nav /> */}
+
         {/* BrowserRouter is a react component and when we create an instance of it, it automatically creates the history component that will track of the address bar in our browser */}
 
         <br />
-        {/* <div> */}
+
         {/*ROUTES will contain the <Route>s that will match the specific path to the component that should be rendered */}
         <Routes>
           <Route path="/" element={<StreamList />} />{" "}
@@ -49,7 +27,6 @@ const App = () => {
           <Route path="/streams/delete" element={<StreamDelete />} />
           <Route path="/streams/show" element={<StreamShow />} />
         </Routes>
-        {/* </div> */}
       </BrowserRouter>
     </div>
   );
