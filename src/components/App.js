5ecc93ee2c19ffa3705e83f64a61a118ef992a7d
Yohanes <yohanesmulugeta21@gmail.com>
2022-05-27 15:44:58 +0300

    test

--- src/components/App.js
+++ src/components/App.js
@@ -15,16 +15,18 @@ const PageTwo = () => {
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div>
-        {/*ROUTES will contain the <Route>s that will match the specific path to the component that should be rendered */}
-        <Routes>
-          <Route exact path="/" element={<PageOne />} />{" "}
-          {/*in the element's prop we pass the component we want to render */}
-          <Route path="/pagetwo" element={<PageTwo />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div>
+      <BrowserRouter>
+        <div>
+          {/*ROUTES will contain the <Route>s that will match the specific path to the component that should be rendered */}
+          <Routes>
+            <Route exact path="/" element={<PageOne />} />{" "}
+            {/*in the element's prop we pass the component we want to render */}
+            <Route path="/pagetwo" element={<PageTwo />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
   );
 };
 
