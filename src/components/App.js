36af88658ce3f7406fd117ecea2d75be2305166f
Yohanes <yohanesmulugeta21@gmail.com>
2022-05-27 15:43:39 +0300

    Test React-Router-Dom with simple example Done!

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom"; //
+
+const PageOne = () => {
+  return <div>Page One</div>;
+};
+
+const PageTwo = () => {
+  return (
+    <div>
+      <h1>Page Two</h1>
+    </div>
+  );
+};
 
 const App = () => {
-  return <h1>App</h1>;
+  return (
+    <BrowserRouter>
+      <div>
+        {/*ROUTES will contain the <Route>s that will match the specific path to the component that should be rendered */}
+        <Routes>
+          <Route exact path="/" element={<PageOne />} />{" "}
+          {/*in the element's prop we pass the component we want to render */}
+          <Route path="/pagetwo" element={<PageTwo />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
