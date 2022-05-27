d7db0b6d845f3e2b7c2bebb26b0132ca6b8743ed
Yohanes <yohanesmulugeta21@gmail.com>
2022-05-27 17:37:07 +0300

    hook the components with the route to display the right component

--- src/components/App.js
+++ src/components/App.js
@@ -5,27 +5,25 @@ import StreamShow from "./streams/StreamShow";
 import StreamCreate from "./streams/StreamCreate";
 import StreamEdit from "./streams/StreamEdit";
 import StreamDelete from "./streams/StreamDelete";
-
-const PageOne = () => {
-  return <div>Page One</div>;
-};
-
-const PageTwo = () => {
-  return (
-    <div>
-      <h1>Page Two</h1>
-    </div>
-  );
-};
+import StreamList from "./streams/StreamList";
 
 const Nav = () => {
   return (
     <ul style={{ listStyle: "none" }}>
       <li>
-        <Link to="/">PageOne</Link>
+        <Link to="/">Streams</Link>
       </li>
       <li>
-        <Link to="/pagetwo">PageTwo</Link>
+        <Link to="/streams/new">Create a Stream</Link>
+      </li>
+      <li>
+        <Link to="/streams/edit">Edit</Link>
+      </li>
+      <li>
+        <Link to="/streams/delete">Delete</Link>
+      </li>
+      <li>
+        <Link to="/streams/show">Show</Link>
       </li>
     </ul>
   );
@@ -41,9 +39,12 @@ const App = () => {
         <div>
           {/*ROUTES will contain the <Route>s that will match the specific path to the component that should be rendered */}
           <Routes>
-            <Route path="/" element={<PageOne />} />{" "}
+            <Route path="/" element={<StreamList />} />{" "}
             {/*in the element's prop we pass the component we want to render */}
-            <Route path="/pagetwo" element={<PageTwo />} />
+            <Route path="/streams/new" element={<StreamCreate />} />
+            <Route path="/streams/edit" element={<StreamEdit />} />
+            <Route path="/streams/delete" element={<StreamDelete />} />
+            <Route path="/streams/show" element={<StreamShow />} />
           </Routes>
         </div>
       </BrowserRouter>
