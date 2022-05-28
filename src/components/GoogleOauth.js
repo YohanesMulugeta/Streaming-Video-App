e3085a7ccfc602b59647baf342448c30cafd4ade
Yohanes <yohanesmulugeta21@gmail.com>
2022-05-28 13:40:51 +0300

    change old OAuth with the new google identity servises

--- src/components/GoogleOauth.js
+++ src/components/GoogleOauth.js
@@ -1,15 +1,29 @@
 import React, { useEffect, useState } from "react";
 
 const GoogleOauth = () => {
-  //   let auth = {};
+  let client = {};
+  let access_token = {};
+  //   let token;
+
+  const [token, setToken] = useState(null);
 
   //   const [isSignedIn, setSignedIn] = useState(null);
   // Initializing the Library
   useEffect(() => {
-    window.gapi.load("client:auth2", () => {
-      console.log(window.gapi);
-    }); // the call back is going to be called when the auth2 is LOADED successfully
-    // console.log(window.gapi  );
+    client = window.google.accounts.oauth2.initTokenClient({
+      client_id:
+        "252766093641-c780p8g3i3e79o2v6uvc46vqjv8dg2mj.apps.googleusercontent.com",
+      scope: "email",
+      callback: (tokenResponse) => {
+        console.log(tokenResponse);
+        token = tokenResponse.access_token;
+        // console.log(token);
+      },
+    });
+
+    client.requestAccessToken();
+
+    console.log(client.i);
   }, []);
 
   return <div>Google Oauth</div>;
