e5cedb20713d0c9dde1f05e9b6b1025183962429
Yohanes <yohanesmulugeta21@gmail.com>
2022-06-05 21:01:29 +0300

    make stream create an isolated component that works by its own

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,17 +1,26 @@
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
+import StreamForm from "../StreamForm";
+import { fetchStream } from "../../actions";
 
 const StreamEdit = () => {
   // this is the new React-router-dom V6 method of using params in the routes element
   // to use locatio we can also do like location = useLocaton()
   const { id } = useParams();
+  const dispach = useDispatch();
 
   const selectStream = (state) => {
     return state.streams[id];
   };
 
   const data = useSelector(selectStream);
+
+  useEffect(() => {
+    if (!data) dispach(fetchStream(id));
+  }, [dispach]);
 
   console.log(data);
 
