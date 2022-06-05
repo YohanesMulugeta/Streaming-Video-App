554c8725c86d90b211c21ade0afddb5811ad5bd9
Yohanes <yohanesmulugeta21@gmail.com>
2022-06-05 22:48:57 +0300

    change StreamCreate to functional component to use useNavigate Hook

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -5,9 +5,10 @@ import { useNavigate } from "react-router-dom";
 // -------------------------------------------------- Redux-Form
 // Field is Capital because it is a react component
 
-class StreamCreate extends React.Component {
-  renderError({ touched, error }) {
-    // console.log(error);
+const StreamCreate = (props) => {
+  const navigate = useNavigate();
+
+  const renderError = ({ touched, error }) => {
     if (touched && error) {
       return (
         <div className="ui error message">
@@ -15,51 +16,42 @@ class StreamCreate extends React.Component {
         </div>
       );
     }
-  }
+  };
 
-  renderInput = ({ input, label, meta }) => {
-    // console.log(input);
-    // console.log(meta);
+  const renderInput = ({ input, label, meta }) => {
     return (
       <div className="field">
         <label>{label}</label>
         <input {...input} autoComplete="off" />{" "}
         {/* // this will put all the key value pairs as a prop
         to the input component */}
-        <div>{this.renderError(meta)}</div>
+        <div>{renderError(meta)}</div>
       </div>
     );
   };
 
-  onSubmit = (formValues) => {
-    this.props.createStream(formValues, this.props.navigate);
+  const onSubmit = (formValues) => {
+    props.createStream(formValues, navigate);
   };
 
-  render() {
-    // console.log(this.props);
+  const render = () => {
     return (
       <form
-        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        onSubmit={props.handleSubmit(onSubmit)}
         // {/* handleSubmit is a function provided by the redux-form through the reduxForm connect function */}
         className="ui form error"
       >
         {/* ------------------------------------------------------------------------- any prop we pass to the Field component other than those recognised by redux-form will be passed to the the component passed to  component prop */}
-        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field name="title" component={renderInput} label="Enter Title" />
         <Field
           name="description"
-          component={this.renderInput}
+          component={renderInput}
           label="Enter Description"
         />
         <button className="ui button primary">Submit</button>
       </form>
     );
-  }
-}
-
-// ------------------------------------------------------ wrapping the stream create component to use useNavigation hook
-const WraperFun = (props) => {
-  const navigate = useNavigate();
-  return <StreamCreate {...props} navigate={navigate} />;
+  };
 };
 
-export default WraperFun;
+export default StreamCreate;
