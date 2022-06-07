f02d4fc03c7356a5e5e1de2cfb5e31ea0db2f383
Yohanes <yohanesmulugeta21@gmail.com>
2022-06-07 12:18:07 +0300

    put some jsx to display nice modal and write some js to hide and display the modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,16 +1,49 @@
 import React from "react";
 import { createPortal } from "react-dom";
+import { useNavigate } from "react-router-dom";
 
-const Mod = () => {
+const ModalComponent = (props) => {
+  const navigate = useNavigate();
+
+  const hider = () => {
+    document.querySelector(".modals").classList.remove("active");
+    navigate("/");
+  };
+
+  //  --------------------------------------------- render
   return (
-    <div className="ui dimmer modals active visible">
-      <div className="ui standard modal visible active">I am the BEST</div>
+    <div onClick={hider} className="ui dimmer modals active visible">
+      <div
+        //   --------------------------------------------------------------------- STOPING the EVENT PROPAGATION
+        onClick={(e) => e.stopPropagation()}
+        className="ui standard modal visible active"
+      >
+        <div className="header">Delete Stream</div>
+
+        <div className="content">Are you sure?</div>
+
+        <div
+          className="actions"
+          onClick={(e) => {
+            //  ----------------------------------------------------------- GUARD key
+            if (!e.target.closest(".button")) return;
+            hider();
+          }}
+        >
+          <button className=" ui button">Cancel</button>
+          <button className="ui primary button">Delete</button>
+        </div>
+      </div>
     </div>
   );
 };
 
+// -------------------------------------------------------------------------- USING PORTAL
 const Modal = (props) => {
-  return createPortal(<Mod />, document.getElementById("modal"));
+  return createPortal(
+    <ModalComponent {...props} />,
+    document.getElementById("modal")
+  );
 };
 
 export default Modal;
